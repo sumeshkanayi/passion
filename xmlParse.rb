9f65df2a738f158719ef080c1cfb9b7c28907539
Soumya Nair <soumyanair@Soumyas-MacBook-Pro.local>
2017-05-03 09:00:33 +0530

    Modified files

--- xmlParse.rb
+++ xmlParse.rb
@@ -2,6 +2,10 @@
         xml = ::File.open("web.xml", 'rb').read
         document = REXML::Document.new(xml)
         document.context[:attribute_quote] = :quote
-	xmlNode=REXML::XPath.each(document,'/web-app[@xmlns="http://java.sun.com/xml/ns/javaee"]/servlet[2]') 
-puts xmlNode
+	xmlNode=REXML::XPath.first(document,'/web-app[@xmlns="http://java.sun.com/xml/ns/javaee"]/servlet[2]') 
+newInitParam=xmlNode.add_element 'init-param'
+(newInitParam.add_element 'param-name').text="enablePooing"
+(newInitParam.add_element 'param-value').text="false"
 
+#,{'param-name'=>'enablePooling', 'param-value'=>'false'}
+puts newInitParam.to_s()
