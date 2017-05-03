ef451c305962a0b4b845e6c328d44c30bbd65600
Soumya Nair <soumyanair@Soumyas-MacBook-Pro.local>
2017-05-03 08:27:39 +0530

    Added lines to print xml

--- xmlParse.rb
+++ xmlParse.rb
@@ -1,16 +1,9 @@
-    standard_server_directory = vanilla_jboss.standard_server_directory
-    file "#{server_directory}/deploy/jbossweb.sar/server.xml" do
-      content lazy {
         require 'rexml/document'
-        xml = ::File.open("#{standard_server_directory}/deploy/jbossweb.sar/server.xml", 'rb').read
+        xml = ::File.open("web.xml", 'rb').read
         document = REXML::Document.new(xml)
         document.context[:attribute_quote] = :quote
-        REXML::XPath.each(document, '//Server//Service//Connector[@protocol="HTTP/1.1"]') do |node|
-          connector = node.attributes
-          connector['maxThreads'] = 250
-          connector['minSpareThreads'] = 25
-          connector['maxSpareThreads'] = 75
-        end
-        document.to_s
-      }
-    end
+	REXML::XPath.each(document,'/web-app[@xmlns="http://java.sun.com/xml/ns/javaee"]/servlet[2]') do |node|
+
+puts node
+
+end
