a6c517d64b9a0c5143d72228c0938e598233013f
Soumya Nair <soumyanair@Soumyas-MacBook-Pro.local>
2017-05-03 08:34:13 +0530

    Added lines to print xml

--- xmlParse.rb
+++ xmlParse.rb
@@ -2,8 +2,6 @@
         xml = ::File.open("web.xml", 'rb').read
         document = REXML::Document.new(xml)
         document.context[:attribute_quote] = :quote
-	REXML::XPath.each(document,'/web-app[@xmlns="http://java.sun.com/xml/ns/javaee"]/servlet[2]') do |node|
+	xmlNode=REXML::XPath.each(document,'/web-app[@xmlns="http://java.sun.com/xml/ns/javaee"]/servlet[2]') 
+puts xmlNode
 
-puts node
-
-end
