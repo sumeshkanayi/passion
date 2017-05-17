2bf6c9c4584f2898a58d14767158d1ee4e7fd600
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-17 07:04:00 +0530

    Update xmlParse.rb

--- xmlParse.rb
+++ xmlParse.rb
@@ -4,5 +4,5 @@ document = REXML::Document.new(xml)
 document.context[:attribute_quote] = :quote
 xmlNode=REXML::XPath.first(document,'/config/scope/application[@extends="webtop/app.xml"]/authentication/xl_sso_config/docbase')
 
-xmlNode.text="sachin"
+xmlNode.text="VALUE TO BE INSERTED"
 puts document.to_s()
